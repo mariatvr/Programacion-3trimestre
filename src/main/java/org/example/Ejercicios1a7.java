ec47ba918a7cfcc88944bdfe8e133216ebb2e79e
mariatvr <mterv12@hotmail.com>
2026-03-12 08:56:48 +0100

    En proceso ejercicios del 1 al 7, voy por el 4

---
+++ src/main/java/org/example/Ejercicios1a7.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Ejercicios1a7 {
+    //EJERCICIO 1
+    public static void escribirNaturales() {
+        try {
+            PrintWriter pw = new PrintWriter(
+                    new BufferedWriter(new FileWriter("/home/marvicrui/DAW_mariateresa/ficheros/numNaturales.txt")));
+            for (int i = 0; i <= 100; i++) {
+                pw.println(i);
+            }
+            pw.close();
+        } catch (IOException e) {
+            System.err.println("Error al escribir en el archivo: " + e.getMessage());
+        }
+    }
+
+    //EJERCICIO 2
+    public static int obtenerSumaNumerosArchivo(String nombreArchivo) {
+        int suma = 0;
+        File f = new File(nombreArchivo);
+
+        try (Scanner entrada = new Scanner(f)) {
+            // Lectura con Scanner como en la página 6 de tus apuntes
+            while (entrada.hasNextInt()) {
+                int numero = entrada.nextInt();
+                suma = suma + numero;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return suma;
+    }
+
+    //Metodo auxiliar
+    private static boolean esPrimo(int numero) {
+        if (numero <= 1) return false;
+        for (int i = 2; i <= numero / 2; i++) {
+            if (numero % i == 0) return false;
+        }
+        return true;
+    }
+
+    //EJERCICIO 3 (lo de leer el fichero y mostrar por pantalla lo junto con el 7)
+    public static void escribirPrimos() {
+        try {
+            PrintWriter pw = new PrintWriter(
+                    new BufferedWriter(new FileWriter("/home/marvicrui/DAW_mariateresa/ficheros/primos.txt")));
+            for (int i = 1; i <= 500; i++) {
+                if (esPrimo(i)) pw.println(i);
+            }
+            pw.close();
+            System.out.println("Fichero primos.txt escrito.");
+        } catch (IOException e) {
+            System.err.println("Error al escribir en el archivo: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        escribirNaturales();
+        int resultadoSuma = obtenerSumaNumerosArchivo("/home/marvicrui/DAW_mariateresa/ficheros/numNaturales.txt");
+        System.out.println("La suma de los números es: " + resultadoSuma);
+    }
+}
